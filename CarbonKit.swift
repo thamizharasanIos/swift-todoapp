d295c435ba6a4ea8b1fa54600edd306a409ecb82
ThamizharasanT <96743694+ThamizharasanT@users.noreply.github.com>
2022-04-06 12:16:26 +0530

    Update CarbonKit.swift

--- CarbonKit.swift
+++ CarbonKit.swift
@@ -1,14 +1,31 @@
+
 import UIKit
 import CarbonKit
 
 class CameraViewController: UIViewController,CarbonTabSwipeNavigationDelegate {
-    var controllernames = ["Features","Products"]
+    var controllernames = ["plain","Texture","Wallpaper","Abstract"]
     var carbonTabSwipeNavigation = CarbonTabSwipeNavigation()
     override func viewDidLoad() {
         super.viewDidLoad()
         
        carbonTabSwipeNavigation = CarbonTabSwipeNavigation(items: controllernames, delegate: self)
         carbonTabSwipeNavigation.insert(intoRootViewController: self)
+         
+        carbonTabSwipeNavigation.setTabBarHeight(36)
+        
+        carbonTabSwipeNavigation.carbonSegmentedControl?.backgroundColor = UIColor.black
+        
+        carbonTabSwipeNavigation.setIndicatorColor(UIColor.white)
+        carbonTabSwipeNavigation.setIndicatorHeight(2)
+        
+        carbonTabSwipeNavigation.setSelectedColor(UIColor.white)
+        carbonTabSwipeNavigation.setNormalColor(UIColor.gray)
+        
+        
+        carbonTabSwipeNavigation.carbonSegmentedControl?.setWidth(view.frame.width / 4, forSegmentAt: 0)
+        carbonTabSwipeNavigation.carbonSegmentedControl?.setWidth(view.frame.width / 4, forSegmentAt: 1)
+        carbonTabSwipeNavigation.carbonSegmentedControl?.setWidth(view.frame.width / 4, forSegmentAt: 2)
+        carbonTabSwipeNavigation.carbonSegmentedControl?.setWidth(view.frame.width / 4, forSegmentAt: 3)
        
     }
     func carbonTabSwipeNavigation(_ carbonTabSwipeNavigation: CarbonTabSwipeNavigation, viewControllerAt index: UInt) -> UIViewController {

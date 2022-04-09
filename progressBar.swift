46c254a56cf66a4cdfce66e22f42aad893b6dabc
ThamizharasanT <96743694+ThamizharasanT@users.noreply.github.com>
2022-04-09 16:19:24 +0530

    Update progressBar.swift

--- progressBar.swift
+++ progressBar.swift
@@ -66,3 +66,17 @@ extension HomeImgViewController:UICollectionViewDelegate,UICollectionViewDataSou
 struct Img {
     var image: String
 }
+//ProgressBar
+class ProgressBar: UIProgressView{
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    let maskLayerPath = UIBezierPath(roundedRect: bounds, cornerRadius: 4.0)
+    let maskLayer = CAShapeLayer()
+    maskLayer.frame = self.bounds
+    maskLayer.path = maskLayerPath.cgPath
+    layer.mask = maskLayer
+     
+  }
+}
+
+
